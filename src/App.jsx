42d4e6cd4ab23e81708c804fbb34ce146dc5fe83
XasanovBoburjon0922 <xasanovboburr0922@gmail.com>
2025-08-22 14:02:19 +0500

    gogogogog

--- src/App.jsx
+++ src/App.jsx
@@ -15,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/verify" element={<VerifyPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
         <Route
           path="/dashboard"
           element={
